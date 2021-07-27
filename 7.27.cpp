cee10013eba5b509c1a5bc7f69e567ddf0a430be
unkhmilu <87937640+unkhmilu@users.noreply.github.com>
2021-07-27 21:09:47 +0800

    Add files via upload

---
+++ 7.27.cpp
@@ -0,0 +1,173 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+struct Book
+{
+   char name[20];
+   short price;
+};
+
+int main()
+{
+
+ //int age = 10;
+   //if (age < 18)
+   //   printf("未成年\n");
+   //else if(age>=18 && age <28)
+   //   printf("青少年\n");
+   //else if(age >= 28 && age < 50)
+   //   printf("壮年\n");
+   //else if( age >= 50 && age < 90)
+   //   printf("老年\n");
+   //else 
+   //   printf("老不死的\n");	
+
+
+   // int age = 40;
+   // if (age < 18)
+   //    printf("未成年\n");
+   // else {
+   //    if(age>=18 && age <28)
+   //    printf("青少年\n");
+   //    else if(age >= 28 && age < 50)
+   //    printf("壮年\n");
+   //    else if( age >= 50 && age < 90)
+   //    printf("老年\n");
+   //    else 
+   //    printf("老不死的\n");
+   // }
+
+	  // int a= 0;
+   //int b = 2;
+   //if(a == 1)
+   //   if (b ==2)
+   //      printf("hehe\n");
+   //else 
+   //   printf("haha\n");
+
+   //   int num =4 ;
+   //if(num = 5){
+   //   printf("haha\n");
+   //}
+
+   // int num =4 ;
+   // if(5 == num){
+   //    printf("haha\n");
+   // }
+
+	   // int  a = 51;
+   // if ( a % 2 == 1){
+   //    printf("yes\n");
+   // }
+   // else
+   //    printf("no\n");
+
+    //int i;
+    //for (i = 0;i<= 100 ; i++ ){
+    //   if ( i % 2 == 1){
+    //      printf("%d\n", i);
+    //   }
+    //   else
+    //   printf("");
+    //}
+
+	   // int num =4 ;
+   // if(num = 5){
+   //    printf("haha\n");
+   // }
+
+   // int num =4 ;
+   // if(5 == num){
+   //    printf("haha\n");
+   // }
+
+   // int  a = 51;
+   // if ( a % 2 == 1){
+   //    printf("yes\n");
+   // }
+   // else
+   //    printf("no\n");
+
+   // int i;
+   // for (i = 0;i<= 100 ; i++ ){
+   //    if ( i % 2 == 1){
+   //       printf("%d\n", i);
+   //    }
+   //    else
+   //    printf("");
+   // }
+
+   //int day = 0;
+   //scanf("%d", &day);
+   //switch (day)
+   //{
+   //case 1:
+   //   printf("星期一\n");
+   //   break;
+   //case 2:
+   //   printf("星期二\n");
+   //   break;
+   //case 3:
+   //   printf("星期三\n");
+   //   break;
+   //case 4:
+   //   printf("星期四\n");
+   //   break;
+   //case 5:
+   //   printf("星期五\n");
+   //   break;
+   //case 6:
+   //   printf("星期路\n");
+   //   break;
+   //case 7:
+   //   printf("星期日\n");
+   //   break;
+   //}
+
+   //   int day = 0;
+   //scanf("%d", &day);
+   //switch (day)
+   //{
+   //case 1:
+   //case 2:
+   //case 3:
+   //case 4:
+   //case 5:
+   //   printf("工作日\n");
+   //   break;
+   //case 6:
+   //case 7:
+   //   printf("休息日\n");
+   //   break;
+   //default:
+   //   printf("输入错误\n");
+   //   break;
+   //}
+
+	// int i =1;
+   //while (i<=10)
+   //{
+   //   printf("%d\n",i);
+   //   i++;
+   //}
+
+   //int i =1;
+   //while (i<=10)
+   //{
+   //   if( i == 5){
+   //      break;
+   //   }
+   //   printf("%d\n",i);
+   //   i++;
+   //}
+
+   
+   int ch = 0;
+   while ((ch = getchar()) != EOF)
+   {
+      putchar (ch);
+   }
+   
+   return 0;
+}
